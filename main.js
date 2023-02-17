ea52ee1b9e9c68c7a8392ba5da333fee4184517a
kchsu <kevin3117213@gmail.com>
2023-02-17 20:55:34 +0800

    response status and time printed

--- main.js
+++ main.js
@@ -6,9 +6,21 @@ const queryParamsContainer = document.querySelector('[data-query-params]')
 const requestHeadersContainer = document.querySelector('[data-request-headers]')
 const keyValueTemplate = document.querySelector('[data-key-value-template]')
 const form = document.querySelector('[data-form]')
+const responseHeadersContainer = document.querySelector('[data-response-headers]')
 
 queryParamsContainer.append(createKeyValuePair())
 requestHeadersContainer.append(createKeyValuePair())
+
+axios.interceptors.request.use( request => {
+  request.customData = request.customData || {}
+  request.customData.startTime = new Date().getTime()
+
+  return request
+})
+
+axios.interceptors.response.use(updateEndTime, e => {
+  return Promise.reject(updateEndTime(e.response))
+})
 
 form.addEventListener('submit', e => {
   e.preventDefault()
@@ -18,7 +30,17 @@ form.addEventListener('submit', e => {
     method: document.querySelector('[data-method]').value,
     params: keyValuePairsToObjects(queryParamsContainer),
     headers: keyValuePairsToObjects(requestHeadersContainer),
-  }).then(resp => console.log(resp))
+  })
+  .catch(e => e)
+  .then(resp => {
+    document.querySelector('[data-response-section]').classList.remove('d-none')
+
+    updateResponseDetails(resp)
+    // updateResponseEditor(resp.data)
+    updateResponseHeaders(resp.headers)
+
+    console.log(resp)
+  })
 })
 
 document.querySelector('[data-add-query-params-btn')
@@ -50,4 +72,29 @@ function keyValuePairsToObjects(container) {
     if (key === '') return data
     return { ...data, [key]: value }
   }, {})
+}
+
+function updateResponseHeaders(headers) {
+  responseHeadersContainer.innerHTML = ''
+  Object.entries(headers).forEach(([key, value]) => {
+    const keyElement = document.createElement('div')
+    keyElement.textContent = key
+    responseHeadersContainer.append(keyElement)
+    
+    const valueElement = document.createElement('div')
+    valueElement.textContent = value
+    responseHeadersContainer.append(valueElement)
+  })
+}
+
+function updateResponseDetails(response) {
+  document.querySelector('[data-status]').textContent = response.status
+  document.querySelector('[data-time]').textContent = response.customData.time
+}
+
+function updateEndTime(response) {
+  response.customData = response.customData || {}
+  response.customData.time = new Date().getTime() - response.config.customData.startTime
+
+  return response
 }
